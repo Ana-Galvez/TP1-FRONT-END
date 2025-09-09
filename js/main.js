01eb815d431fdc74f577e601ec5634908ca654ed
anamarigalvez1688@gmail.com <amg1688>
2025-09-09 18:43:40 -0300

    feat: agregado menú hamburguesa < 400px

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
+// Función para modo oscuro/claro
 function toggleTheme(){
   document.body.style.background =
     document.body.style.background=='black' ? '#f7f7fb' : 'black';
 }
 
+// Función para alternar el menú hamburguesa
+function toggleMenu() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.classList.toggle('active');
+}
+
+
+// Función para filtrar integrantes
 function filterTeam(){
   const q=document.getElementById('filter-input').value.toLowerCase();
   document.querySelectorAll('.member').forEach(m=>{
@@ -11,10 +20,12 @@ function filterTeam(){
   });
 }
 
+// Función para ocultar/mostrar habilidades
 function toggleSkills(){
   document.getElementById('skills-list').classList.toggle('hidden');
 }
 
+//Función para agregar a la lista de la bitácora (no guarda datos)
 function addBitacoraEntry(){
   const input=document.getElementById('bitacora-input');
   const list=document.getElementById('bitacora-list');
@@ -25,7 +36,7 @@ function addBitacoraEntry(){
   input.value='';
 }
 
-
+//Función para mostrar el alert del footer
 document.querySelector("footer").addEventListener("click", () => {
   alert("🎮 ¡Modo secreto activado! Gracias por visitar nuestra web.");
 });
