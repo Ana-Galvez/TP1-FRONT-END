357a7d78de1d8b0f5bf69cc06b906bdfa39a1912
Jessicaoleszuk <115583021+Jessicaoleszuk@users.noreply.github.com>
2025-09-09 21:03:28 -0300

    actualizacion css y js jessica

---
+++ js/jes.js
@@ -0,0 +1,8 @@
+function toggleMovies() {
+  const MoviesList = document.getElementById('Movies-list');
+  MoviesList.style.display = MoviesList.style.display === 'none' ? 'block' : 'none';
+}
+function toggleMusic() {
+  const MusicList = document.getElementById('Music-list');
+  MusicList.style.display = MusicList.style.display === 'none' ? 'block' : 'none';
+}
