0e3766c5b1ed4421b321939b596ac30cb2921b2c
Jessicaoleszuk <115583021+Jessicaoleszuk@users.noreply.github.com>
2025-09-09 22:44:33 -0300

    actualizacion js jessica

--- js/jes.js
+++ js/jes.js
@@ -1,3 +1,17 @@
+const quotes = [
+  "El éxito es la suma de pequeños esfuerzos repetidos día tras día.",
+  "Cree en ti y todo será posible.",
+  "La disciplina es el puente entre metas y logros.",
+  "Cada día es una nueva oportunidad para crecer.",
+  "Haz hoy lo que tu futuro yo te agradecerá."
+];
+
+function newQuote() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  document.getElementById('quote').textContent = quotes[randomIndex];
+}
+
+
 function toggleMovies() {
   const MoviesList = document.getElementById('Movies-list');
   MoviesList.style.display = MoviesList.style.display === 'none' ? 'block' : 'none';
@@ -6,3 +20,4 @@ function toggleMusic() {
   const MusicList = document.getElementById('Music-list');
   MusicList.style.display = MusicList.style.display === 'none' ? 'block' : 'none';
 }
+
